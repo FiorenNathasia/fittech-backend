5603628a5951745e2dbb25e8709e442306faa8bc
FiorenNathasia <fiorennathasia@gmail.com>
2024-06-06 19:10:36 +0100

    Start login api.

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require("express");
 const path = require("path");
 const mysql = require("mysql");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config({ path: "./.env" });
 
 const app = express();
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const db = mysql.createConnection({
   host: process.env.DATABASE_HOST,
@@ -16,6 +21,7 @@ const db = mysql.createConnection({
 const publicDirectory = path.join(__dirname);
 app.use(express.static(publicDirectory));
 app.set("view engine", "hbs");
+
 db.connect((error) => {
   if (error) {
     console.log(error);
@@ -24,11 +30,35 @@ db.connect((error) => {
   }
 });
 
+app.post("/signup", (req, res) => {
+  const sql = "INSERT INTO users (`name`, `email`, `password`) VALUES (?)";
+  const values = [req.body.name, req.body.email, req.body.password];
+  db.query(sql, [values], (error, data) => {
+    if (error) {
+      return res.status(500).json({ error: "Error inserting data" });
+    }
+    return res.status(200).json(data);
+  });
+});
+
+app.post("/login", (req, res) => {
+  const sql = "SELECT * FROM users WHERE `email`= ? AND `password` = ?";
+  db.query(sql, [req.body.email, req.body.password], (error, data) => {
+    if (error) {
+      return res.status(500).json({ error: "Error inserting data" });
+    }
+    if (data.length > 0) {
+      return res.json("Succes");
+    } else {
+      return res.json("Fail");
+    }
+  });
+});
+
 app.get("/", (req, res) => {
-  // res.send("<h1>HomePage</h1>");
   res.render("index");
 });
 
-app.listen(5001, () => {
-  console.log("Server started on PORT 5001");
+app.listen(8081, () => {
+  console.log("Server started on PORT 8081");
 });
