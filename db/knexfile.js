19e23c091a98525c3e448a934cea6f96da43278e
FiorenNathasia <fiorennathasia@gmail.com>
2024-12-25 17:12:28 +0000

    Practice making db w/ Knex.js

---
+++ db/knexfile.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: {
+      host: "localhost",
+      user: "fiorennathasia",
+      password: "gayagaya56781234",
+      database: "knex_tutorial",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
