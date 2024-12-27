74dd855737142223f39a6fddabe5a6945835ec62
FiorenNathasia <fiorennathasia@gmail.com>
2024-12-27 18:54:59 +0000

    GET user Api call made

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require("express");
-const router = require("./routes");
+// const router = require("./routes/auth");
+const db = require("./db/db");
 
 const app = express();
 app.use(express.json());
-app.use(router);
+// app.use(router);
 
-app.listen(8080, () => console.log("server listening on port 8080"));
+app.get("/login", async (req, res) => {
+  const userEmail = req.query.email;
+  const userPassword = req.query.password;
+
+  const result = await db("users")
+    .where("email", userEmail)
+    .andWhere("password", userPassword);
+
+  console.log(result);
+  res.status(200).send({ mesage: "hello" });
+});
+
+app.listen(8080, () => {
+  console.log("server listening on 8080");
+});
---
+++ routes/auth.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const usersController = require("../controller/users");
+
+const router = express.Router();
+
+module.exports = router;
