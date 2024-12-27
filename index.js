bf2560db42e3e6019f8c1a1ffc01675af8c51377
FiorenNathasia <fiorennathasia@gmail.com>
2024-12-27 22:53:21 +0000

    Finish login api endpoint

--- index.js
+++ index.js
@@ -12,10 +12,34 @@ app.get("/login", async (req, res) => {
 
   const result = await db("users")
     .where("email", userEmail)
-    .andWhere("password", userPassword);
+    .andWhere("password", userPassword)
+    .first();
+
+  // if we found someone result = [{id:1, first_name ...}]
+  // if we cant find someone result = []
+
+  // if we put .first() at the end, then we always get the first one in the list
+
+  // if we found someone result = {id: 1, first_name ...}
+  // if we cant find someone result = undefined
+
+  // so therefore, when we use .first - result is no longer a list
+  // so doing result.length - doesn\t make any sense, you cant do .length on
+  // something that isn't a list
 
   console.log(result);
-  res.status(200).send({ mesage: "hello" });
+
+  if (result) {
+    res.status(200).send({
+      data: {
+        firstName: result.first_name,
+        lastName: result.last_name,
+        email: result.email,
+      },
+    });
+  } else {
+    res.status(403).send({ message: "You're unauthorized" });
+  }
 });
 
 app.listen(8080, () => {
