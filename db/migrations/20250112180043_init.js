c33772240b799e11ea5bbc78677425e82ad9337c
FiorenNathasia <fiorennathasia@gmail.com>
2025-01-13 17:50:16 +0000

    Made new workout table

---
+++ db/migrations/20250112180043_init.js
@@ -0,0 +1,28 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("workouts", (table) => {
+    table.increments("id").primary();
+    table
+      .integer("user_id")
+      .unsigned()
+      .references("users.id")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+    table.string("title").notNullable();
+    table.string("steps").notNullable();
+    table.string("url").notNullable();
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("worrkouts");
+};
